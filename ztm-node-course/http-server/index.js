8a4e3cbdbb8079821f0a5b18ac64d745dddf8d48
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-23 11:42:09 +0800

    Introduced some logic for requests

--- ztm-node-course/http-server/index.js
+++ ztm-node-course/http-server/index.js
@@ -1,17 +1,36 @@
 const http = require('http');
 const PORT = 3000;
+const friends = [
+  {
+    id: 0,
+    name: 'Sir Isaac Newton'
+  },
+  {
+    id: 1,
+    name: 'Albert Einstein'
+  },
+];
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/friends'){
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    res.end(JSON.stringify({
-      id: 1,
-      name: 'Sir Isaac Newton'
-    }));
+  const items = req.url.split('/');
+  if(items[1] === 'friends'){
+    // res.writeHead(200, {
+    //   'Content-Type': 'application/json',
+    // });
+    if(Number(items[2]) === 0){
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(friends[0]));
+    }
+    else if(Number(items[2]) === 1){
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(friends[1]));
+    }
   }
-  else if(req.url === '/messages'){
+  else if(items[1] === 'messages'){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>');
     res.write('<ul>');
@@ -21,6 +40,10 @@ const server = http.createServer((req, res) => {
     res.write('</body>');
     res.write('</html>');
     res.end();
+  }
+  else {
+    res.statusCode = 404;
+    res.end()
   }
 });
 
