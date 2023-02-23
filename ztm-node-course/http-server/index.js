c2a73e3ecd9455062bf76b9c769013c77a4ab9fe
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-23 11:23:55 +0800

    Basic http server

---
+++ ztm-node-course/http-server/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/friends'){
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify({
+      id: 1,
+      name: 'Sir Isaac Newton'
+    }));
+  }
+  else if(req.url === '/messages'){
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
