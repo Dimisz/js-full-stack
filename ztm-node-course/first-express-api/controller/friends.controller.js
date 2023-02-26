e7446daf4abf18a38ff4952abec34f16618e4a80
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-26 12:04:37 +0800

    Created a model to simulate a db

--- ztm-node-course/first-express-api/controller/friends.controller.js
+++ ztm-node-course/first-express-api/controller/friends.controller.js
@@ -1,13 +1,4 @@
-const friends = [
-  {
-    id: 0, 
-    name: 'Albert Einstein'
-  },
-  {
-    id: 1, 
-    name: 'Sir Isaac Newton'
-  }
-];
+const model = require('../models/friends.model');
 
 function postFriend(req, res){
   if(!req.body.name) {
@@ -17,19 +8,19 @@ function postFriend(req, res){
   }
   const newFriend = {
     name: req.body.name,
-    id: friends.length
+    id: model.length
   };
-  friends.push(newFriend);
+  model.push(newFriend);
   res.json(newFriend);
 }
 
 function getAllFriends(req, res){
-  res.json(friends);
+  res.json(model);
 }
 
 function getAFriend(req, res){
   const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
+  const friend = model[friendId];
   if(friend){
     res.status(200).json(friend);
   }
