150c3b236ecf70c0061c0d6243fb9e2a51f8ca9a
Vladimir Solovyov <leilyngale@icloud.com>
2023-02-26 10:01:12 +0800

    Added a parametrized call

--- ztm-node-course/first-express-api/server.js
+++ ztm-node-course/first-express-api/server.js
@@ -22,6 +22,19 @@ app.get('/messages', (req, res) => {
   res.send('<h1>Messages</h1>');
 });
 
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if(friend){
+    res.status(200).json(friend);
+  }
+  else {
+    res.status(404).json({
+      error: 'Friend does not exist'
+    });
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
