388fc6317fdfe44ffa12ce8ec8a81d5923792d64
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-12 16:23:39 +0800

    Added a Card component

--- ztm-web-dev/react-lectures/monsters/src/App.js
+++ ztm-web-dev/react-lectures/monsters/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {robots} from './robots';
+
+import Card from './components/Card';
 
 const App = () => {
+  const renderedRobots = robots.map((robot) => {
+    return (<Card 
+              key={robot.id} 
+              id={robot.id}
+              name={robot.name} 
+              email={robot.email} 
+            />);
+  });
+
   return(
-    <h1>Monsters app</h1>
+    <>{renderedRobots}</>
   );
 }
 
