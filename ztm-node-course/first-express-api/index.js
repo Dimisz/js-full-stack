883881d526ae409bae14943f71e8eac81bbc3398
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-14 10:32:49 +0800

    Added basic functionality

--- ztm-node-course/first-express-api/index.js
+++ ztm-node-course/first-express-api/index.js
@@ -3,12 +3,33 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein'
+  },
+  {
     id: 1,
     name: 'Sir Isaac Newton'
-  });
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
 });
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if(friend){
+    res.status(200).json(friend);
+  }
+  else {
+    res.status(404).json({
+      error: 'Friend does not exist'
+    });
+  }
+})
 
 app.get('/messages', (req, res) => {
   res.send('<h1>Hello there</h1>');
