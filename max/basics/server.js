00b03e421a1147973bd4d23777f154091c0ee7c2
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-20 09:31:15 +0800

    Added parsing incoming requests

--- max/basics/server.js
+++ max/basics/server.js
@@ -10,13 +10,26 @@ const server = http.createServer(function(req, res){
   if(url === '/'){
     res.write('<html>');
     res.write('<body>');
-    res.write('<form action="/message" method="POST"><input name="mesage" type="text"><button type="submit">Send</button></form>');
+    res.write('<form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form>');
     res.write('</body>');
     res.write('</html>');
     return res.end();
   }
 
   if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message);
+      fs.writeFileSync('message.txt', message);
+    });
+
     fs.writeFileSync('message.txt', 'Dummy');
     res.statusCode = 302;
     res.setHeader('Location', '/');
