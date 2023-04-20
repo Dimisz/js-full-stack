a46ed2052327467ebab7bee3c95ccf01b8064a40
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-20 12:53:41 +0800

    Added routing

--- max/express-app/app.js
+++ max/express-app/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 const app = express();
+app.use(bodyParser.urlencoded());
 
-app.use((req, res, next) => {
-  console.log('in another middleware');
-  res.send('<h1>Mello from  express</h1>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
---
+++ max/express-app/routes/shop.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+router.get('/', (req, res, next) => {
+  console.log('in another middleware');
+  res.send('<h1>Hell low from  express</h1>');
+});
+
+module.exports = router;
