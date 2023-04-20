733ffd7f04275ee67b141553eed84930b1298936
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-20 10:13:41 +0800

    Separated routes to a separate file

--- max/basics/server.js
+++ max/basics/server.js
@@ -1,49 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
+
 const PORT = 3000;
 
-const server = http.createServer(function(req, res){
-  //console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  const method = req.method;
-
-  if(url === '/'){
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
-  }
-
-  if(url === '/message' && method === 'POST'){
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      console.log(message);
-      fs.writeFileSync('message.txt', message);
-    });
-
-    fs.writeFileSync('message.txt', 'Dummy');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<body>');
-  res.write('<h1>My first page</h1>');
-  res.write('</body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
