da07791d63e7dadc56e0f56214de3b5e08c645c4
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-21 09:05:44 +0800

    Converted the rest html docs to pug

--- max/express-app/routes/admin.js
+++ max/express-app/routes/admin.js
@@ -8,7 +8,8 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
   // console.log('in another middleware');
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  // res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  res.render('add-product', {docTitle: 'Add a product'});
 });
 
 router.post('/add-product', (req, res, next) => {
