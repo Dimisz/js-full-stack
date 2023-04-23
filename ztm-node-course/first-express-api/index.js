15630df0a3bf16a5b89a171a4fbf180939f3aa45
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-23 18:27:59 +0800

    Scaffolded the app

--- ztm-node-course/first-express-api/index.js
+++ ztm-node-course/first-express-api/index.js
@@ -1,44 +1,24 @@
 const express = require('express');
-
 const app = express();
+
 const PORT = 3000;
 
-const friends = [
-  {
-    id: 0,
-    name: 'Albert Einstein'
-  },
-  {
+app.get('/', (req, res, next) => {
+  res.send({
     id: 1,
     name: 'Sir Isaac Newton'
-  }
-];
-
-app.get('/friends', (req, res) => {
-  res.json(friends);
+  });
+  next();
 });
-
-app.get('/friends/:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if(friend){
-    res.status(200).json(friend);
-  }
-  else {
-    res.status(404).json({
-      error: 'Friend does not exist'
-    });
-  }
-})
 
 app.get('/messages', (req, res) => {
-  res.send('<h1>Hello there</h1>');
+  res.send('<h1>Messages page</h1>');
 });
 
-app.post('/', (req,res) => {
-  console.log('Updating messages....');
+app.post('/messages', (req, res) => {
+  console.log('Updating messages...');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`)
-});
+  console.log(`listening on port ${PORT}`);
+})
