e61de9b0ee2c4422709ae222d1adbd43136e98fd
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-24 16:46:49 +0800

    Basic annotations

---
+++ grider/ts/types/variables.ts
@@ -0,0 +1,23 @@
+// arrays
+let colors: string[] = ['red', 'blue', 'yellow'];
+let myNums: number[] = [1, 2, 3];
+
+
+// classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+// functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+  
+}
