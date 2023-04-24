68abce1d4d404908cf5277784b88fd1454a4066d
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-24 15:06:29 +0800

    Starting typescript

---
+++ grider/ts/fetch-json/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url)
+  .then((res) => {
+    const todo = res.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`id: ${id}\ntitle: ${title}\ncompleted: ${completed}.`)
+  });
