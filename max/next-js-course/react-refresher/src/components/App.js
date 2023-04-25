55b8821b5d1d7bccfaca60a328ca3b94509bd98f
Vladimir Solovyov <leilyngale@icloud.com>
2023-04-25 14:47:19 +0800

    Added layout

--- max/next-js-course/react-refresher/src/components/App.js
+++ max/next-js-course/react-refresher/src/components/App.js
@@ -1,14 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 
-import MainNavigation from './layout/MainNavigation';
 import AllMeetupsPage from '../pages/AllMeetupsPage';
 import FavouritesPage from '../pages/FavouritesPage';
 import NewMeetupPage from '../pages/NewMeetupPage';
 
+import Layout from './layout/Layout';
+
 const App = () => {
   return(
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path='/new-meetup' exact>
           <NewMeetupPage />
@@ -20,7 +20,7 @@ const App = () => {
           <AllMeetupsPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
