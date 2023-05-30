85a39fb2a7395597439fd8876ba69916ee03dbd6
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-30 15:41:36 +0800

    Basic routing

--- max/basics/app.js
+++ max/basics/app.js
@@ -2,7 +2,18 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  if(req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body><html>');
+    return res.end();
+  }
+  else if(req.url === '/message'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Message successfully sent!</h1></body></html>');
+    return res.end();
+  }
+
 });
 
 server.listen(3000, ()=>{
