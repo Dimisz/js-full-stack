f58791aea9ffad4992ed4c09905128c65ae2b88a
Vladimir Solovyov <leilyngale@icloud.com>
2023-05-30 16:13:12 +0800

    Parse input using Buffer

--- max/basics/app.js
+++ max/basics/app.js
@@ -1,16 +1,33 @@
 const http = require('http');
+const fs = require('fs');
+
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  if(req.url === '/'){
+  const method = req.method;
+
+  if(url === '/'){
     res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body><html>');
+    res.write('<html><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body><html>');
     return res.end();
   }
-  else if(req.url === '/message'){
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body><h1>Message successfully sent!</h1></body></html>');
+  else if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(`chunk ${chunk}`);
+      body.push(chunk);
+      // fs.writeFileSync('message.txt', chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(`parsed body ${parsedBody}`);
+      const message = parsedBody.split('=')[1].split('+').join(' ');
+      fs.writeFileSync('message.txt', message);
+    })
+    
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
 
