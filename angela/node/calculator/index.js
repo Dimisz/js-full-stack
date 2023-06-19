51de2d5d3441e7095a5b3743aed2a69c6e210c02
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-19 17:10:13 +0800

    Completed the calculator

--- angela/node/calculator/index.js
+++ angela/node/calculator/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app = express();
+// const bodyParser = require('body-parser');
+
 const PORT = 3000;
+
+const app = express();
+// app.use(bodyParser);
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-  res.send('<h1>Calculator App</h1>');
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/', (req, res) => {
+  const sum = (Number(req.body.num1) || 0) + (Number(req.body.num2) || 0);
+  // console.log(req.body);
+  res.send(`<h1>Thanks</h1><h2>The result is: ${sum}</h2>`);
 });
 
 
