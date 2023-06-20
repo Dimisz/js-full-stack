1e6b46ff4374a881204556035f7d94d84ad864d7
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-20 09:13:43 +0800

    Weather app

--- angela/node/weather-app/index.js
+++ angela/node/weather-app/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const https = require('https');
+
 const app = express();
+
 const PORT = 3001;
+const apiId = '#api id goes here';
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&${apiId}&q=`;
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,8 +15,27 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const city = req.body.city;
-  
-  res.send(`<h1>Weather in ${city} is unknown so far...</h1>`);
+  const url = `${BASE_URL}${city}`;
+  https.get(url, (response) => {
+    console.log(response.statusCode);
+    response
+      .on('data', (data) => {
+        const weatherData = JSON.parse(data);
+        const temperature = weatherData.main.temp || "unknown so far...";
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        console.log(temperature);
+        console.log(weatherDescription)
+        res.write(`<p>The current weather is ${weatherDescription}</p>`)
+        res.write(`<h1>Weather in ${city} is ${temperature}C<sup>0</sup>.</h1>`);
+        res.write(`<img src="${imageUrl}" alt="weather icon" >`);
+        res.send();
+      });
+  });
+  // console.log(temperature);
+  // res.send(`<h1>Weather in ${city} is ${temperature}</h1>`);
 
 });
 
