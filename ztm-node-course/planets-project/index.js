8535d726efbcf7595f487c3e08d6a0bef2d1e306
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-20 18:54:27 +0800

    Housekeeping

--- ztm-node-course/planets-project/index.js
+++ ztm-node-course/planets-project/index.js
@@ -1,33 +1,28 @@
 const {parse} = require('csv-parse');
 const fs = require('fs');
 
-const habitablePlanets = [];
-
-function isHabitablePlanet(planet){
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 
-    && planet['koi_insol'] < 1.11 
-    && planet['koi_prad'] < 1.6;
-};
+const results = [];
+const isHabitable = (planet) => {
+  return planet['koi_disposition'] === 'CONFIRMED' &&
+          planet['koi_insol'] > 0.36 &&
+          planet['koi_insol'] < 1.11 &&
+          planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
     comment: '#',
-    columns: true,
-  })) // parsing from bytes to readable format
+    columns: true
+  }))
   .on('data', (data) => {
-    if(isHabitablePlanet(data)){
-      habitablePlanets.push(data);
+    if(isHabitable(data)){
+      results.push(data);
     }
   })
-  .on('error', (error) => {
-    console.log(error);
+  .on('error', (err) => {
+    console.error(err);
   })
   .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    
-    console.log(`${habitablePlanets.length} habitable planets found`);
-    console.log('done processing file!');
+    console.log(results.length);
+    console.log('Done');
   });
