c3e98e9783f557df5875c398bc4dc831dff6533c
Vladimir Solovyov <leilyngale@icloud.com>
2023-06-20 10:58:58 +0800

    Housekeeping

--- angela/node/calculator/index.js
+++ angela/node/calculator/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
 // app.use(bodyParser);
@@ -18,7 +18,20 @@ app.post('/', (req, res) => {
   res.send(`<h1>Thanks</h1><h2>The result is: ${sum}</h2>`);
 });
 
+app.get('/bmi-calculator', (req, res) => {
+  res.sendFile(`${__dirname}/bmi-calculator.html`);
+});
+
+app.post('/bmi-calculator', (req, res) => {
+  const weight = Number(req.body.weight) || 0;
+  const height = Number(req.body.height) || 0;
+  const bmi = weight / (height ** 2);
+  res.send(`<h1>Your BMI is ${bmi}</h1>`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}...`);
 });
+
+// api.openweathermap.org/data/2.5/weather?q=London,uk&appid=47723eb2cd4b90bc7886a4c2cbc2866b
